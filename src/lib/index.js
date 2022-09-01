902ff7e6bc5faa8753173d2eeaba36990a4d59d4
mishkolesnikov <mishkolesnikov@gmail.com>
2022-09-01 19:38:11 +0400

    feat: add bakery compenent using the lib

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,4 @@
-import { Front } from "./badges/cert-membership/front";
-import { Back } from './badges/cert-membership/back';
 import { Printer } from './printer/printer';
 import { badges } from "./badges";
 
-export { Front, Back, Printer, badges };
+export { Printer, badges };
